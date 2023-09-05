ac6d0a8426c51fd3beb2de69c850f6a7c85db5aa
Szymixolotl <szyfra30@gmail.com>
2023-09-05 17:12:30 +0200

    First commit

---
+++ android/app/src/main/kotlin/com/example/kl4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kl4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Szymon apaka"),
+        ),
+        body: Center(
+          child: Text("Przykładowy"),
+        ),
+      ),
+    );
+  }
+}                                                  
